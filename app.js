02895e2f18efe3e97999d63bea436f6590f296dd
Anica Rahman <anicarahman2009@yahoo.it>
2022-11-02 18:29:27 +0000

    js update

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
+//QUESTION
 const questionEl = document.getElementById("question");
 
-const num1 = Math.ceil(Math.random() * 50);
-const num2 = Math.ceil(Math.random() * 50);
+const num1 = Math.floor(Math.random() * 20);
+const num2 = Math.floor(Math.random() * 20);
 
 questionEl.innerText = `What is ${num1} x ${num2}?`;
+
+//INPUT VALUE and SCORE UPDATE
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+  score = 0;
+}
+
+const correctAnswer = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAnswer = +inputEl.value;
+
+  if (userAnswer === correctAnswer) {
+    score++;
+    updateLocalStorage();
+  } else {
+    score--;
+    updateLocalStorage();
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
+
+scoreEl.innerText = `score: ${score}`;
